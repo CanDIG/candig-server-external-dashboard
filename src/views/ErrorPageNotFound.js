7e33b5e9b0f6a472143e8ed52d88668b6c0dc15d
Courtney Gosselin <cgosselin@bcgsc.ca>
2021-08-10 12:15:27 -0700

    fixing style sheet naming to candig-blue
    Signed-off-by: Courtney Gosselin <cgosselin@bcgsc.ca>

--- src/views/ErrorPageNotFound.js
+++ src/views/ErrorPageNotFound.js
@@ -10,14 +10,14 @@ function ErrorPageNotFound() {
   return (
     <>
       <div className="content d-flex flex-column justify-content-center align-items-center">
-        <h1 className="display-3 m-1 text-navy font-gothic textShadow-grey font-weight-bold ">Error 404</h1>
+        <h1 className="display-3 m-1 text-candig-blue font-gothic textShadow-grey font-weight-bold ">Error 404</h1>
         <h5 className="mt-1 mb-3 font-gothic text-center">
           Sorry, the page you requested was
           <b> not found</b>
         </h5>
         <div className="d-flex justify-content-around mt-3">
-          <Link to="/v2/dashboard/overview" className="d-flex justify-content-center navy-button p-2 mr-3 font-gothic font-weight-bold">Overview</Link>
-          <Link to="/v2/dashboard/help" className="d-flex justify-content-center outline-navy-button p-2 mr-3 text-navy font-gothic font-weight-bold">Help</Link>
+          <Link to="/v2/dashboard/overview" className="d-flex justify-content-center candig-blue-button p-2 mr-3 font-gothic font-weight-bold">Overview</Link>
+          <Link to="/v2/dashboard/help" className="d-flex justify-content-center outline-candig-blue-button p-2 mr-3 text-candig-blue font-gothic font-weight-bold">Help</Link>
         </div>
       </div>
     </>
