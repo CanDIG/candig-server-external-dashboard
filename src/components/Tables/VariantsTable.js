1d9042c793261c248d0dd2b193bd58a8f07c95d6
Courtney <37649170+CourtneyGosselin@users.noreply.github.com>
2022-01-14 14:03:38 -0800

    Adding dynamic columns and dropdown (#50)
    * Adding dynamic columns and dropdown
    * Travis fixes
    * Dependency added to use effect
    
    Signed-off-by: Courtney Gosselin <cgosselin@bcgsc.ca>

--- src/components/Tables/VariantsTable.js
+++ src/components/Tables/VariantsTable.js
@@ -12,14 +12,51 @@ import '../../assets/css/VariantsSearch.css';
 function VariantsTable({ rowData, datasetId }) {
   const notifyEl = useRef(null);
 
-  const columnDefs = [
-    { headerName: 'Reference Name', field: 'referenceName' },
-    { headerName: 'Start', field: 'start' },
-    { headerName: 'End', field: 'end' },
-    { headerName: 'Reference Bases', field: 'referenceBases' },
-    { headerName: 'Alternate Bases', field: 'alternateBases' },
-  ];
   let gridOptions = {};
+
+  function getColumnDefs() {
+    const columnDefs = [
+      { headerName: 'Chromosome', field: 'referenceName' },
+      { headerName: 'Start', field: 'start' },
+      { headerName: 'End', field: 'end' },
+      { headerName: 'Reference Bases', field: 'referenceBases' },
+      { headerName: 'Alternate Bases', field: 'alternateBases' },
+    ];
+
+    if (rowData[0] !== undefined) { // First population is empty
+      const { attr } = rowData[0].attributes;
+      Object.keys(attr).forEach((key) => {
+        columnDefs.push({
+          headerName: key,
+          valueFormatter: (params) => {
+            try {
+              let attributeValue;
+
+              if (params.value.values[0].stringValue !== undefined) {
+                attributeValue = params.value.values[0].stringValue;
+              } else if (params.value.values[0].doubleValue !== undefined) {
+                attributeValue = params.value.values[0].doubleValue;
+              } else if (params.value.values[0].int32Value !== undefined) {
+                attributeValue = params.value.values[0].int32Value;
+              }
+
+              return attributeValue;
+            } catch (error) {
+              // console.log(error);
+              /*
+                * This is to handle the case where the attribute value is not available in the select row
+                */
+              return 'N/A';
+            }
+          },
+          field: `attributes.attr.${key}`, // Allows us to work with key without it retroactively changing to the last key
+          editable: true,
+        });
+      });
+    }
+
+    return columnDefs;
+  }
 
   function onSelectionChanged() {
     const selectedRows = gridOptions.api.getSelectedRows();
@@ -73,7 +110,7 @@ function VariantsTable({ rowData, datasetId }) {
       resizable: true,
       filter: true,
       flex: 1,
-      minWidth: 20,
+      minWidth: 150,
       minHeight: 300,
     },
     onSelectionChanged,
@@ -84,6 +121,7 @@ function VariantsTable({ rowData, datasetId }) {
     enableRangeSelection: true,
     paginationAutoPageSize: true,
     pagination: true,
+    valueCache: true,
     frameworkComponents: {
       VariantsTableButton,
     },
@@ -94,7 +132,7 @@ function VariantsTable({ rowData, datasetId }) {
       <NotificationAlert ref={notifyEl} />
       <div className="ag-theme-alpine">
         <AgGridReact
-          columnDefs={columnDefs}
+          columnDefs={getColumnDefs()}
           rowData={rowData}
           gridOptions={gridOptions}
           context={{ datasetId }}
--- src/views/VariantsSearch.js
+++ src/views/VariantsSearch.js
@@ -4,9 +4,12 @@ import {
 } from 'reactstrap';
 
 import { useSelector } from 'react-redux';
+import { MultiSelect } from 'react-multi-select-component';
 
 import VariantsTable from '../components/Tables/VariantsTable';
-import { searchVariant, searchVariantSets, getReferenceSet } from '../api/api';
+import {
+  searchVariant, searchVariantSets, searchVariantByVariantSetIds, getReferenceSet,
+} from '../api/api';
 
 import { notify, NotificationAlert } from '../utils/alert';
 import LoadingIndicator, {
@@ -25,6 +28,9 @@ function VariantsSearch() {
   const [variantSet, setVariantSets] = useState('');
   const [referenceSetName, setReferenceSetName] = useState('');
   const { promiseInProgress } = usePromiseTracker();
+  const [options, setOptions] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const [variantSetIds, setVariantSetIds] = useState([]);
 
   /*
   Fetches reference set Name and sets referenceSetName
@@ -43,6 +49,12 @@ function VariantsSearch() {
     trackPromise(
       searchVariantSets(datasetId).then((data) => {
         setVariantSets(data.results.total);
+        setSelected([]);
+        data.results.variantSets.forEach((variant) => {
+          options.push({ label: variant.name, value: variant.id });
+        });
+        setOptions(options);
+        setSelected(options);
         settingReferenceSetName(data.results.variantSets[0].referenceSetId);
       }).catch(() => {
         setVariantSets('Not Available');
@@ -54,18 +66,35 @@ function VariantsSearch() {
         // );
       }),
     );
-  }, [datasetId]);
+  }, [datasetId, options]);
 
   const formHandler = (e) => {
     e.preventDefault(); // Prevent form submission
 
-    // searchVariant(datasetId, e.target.start.value, e.target.end.value,)
-    searchVariant(datasetId, e.target.start.value, e.target.end.value, e.target.referenceName.value)
-      .then((data) => {
-        setDisplayVariantsTable(true);
+    if (selected) {
+      selected.forEach((variantSetId) => {
+        variantSetIds.push(variantSetId.value);
+      });
+      // searchVariant(e.target.start.value, e.target.end.value, e.target.chromosome.value, variantSetIds) query /variants/search
+      searchVariantByVariantSetIds(e.target.start.value, e.target.end.value, e.target.chromosome.value, variantSetIds)
+        .then((data) => {
+          setDisplayVariantsTable(true);
+          setRowData(data.results.variants);
+        }).catch(() => {
+          setRowData([]);
+          setDisplayVariantsTable(false);
+          //           notify(
+          //             notifyEl,
+          //             'No variants were found.',
+          //             'warning',
+          //           );
+        });
+      setVariantSetIds([]);
+    } else {
+      searchVariant(datasetId, e.target.start.value, e.target.end.value, e.target.chromosome.value).then((data) => {
         setRowData(data.results.variants);
+        setDisplayVariantsTable(true);
       }).catch(() => {
-        setRowData([]);
         setDisplayVariantsTable(false);
         notify(
           notifyEl,
@@ -73,6 +102,7 @@ function VariantsSearch() {
           'warning',
         );
       });
+    }
   };
 
   return (
@@ -80,6 +110,30 @@ function VariantsSearch() {
       <div className="content">
         <NotificationAlert ref={notifyEl} />
         <Row className="justify-content-md-center">
+          <Col lg="4" md="4" sm="4">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="4" xs="5">
+                    <div className="icon-big text-center icon-warning">
+                      <i className="nc-icon nc-map-big text-primary" />
+                    </div>
+                  </Col>
+                  <Col md="8" xs="7">
+                    <div className="numbers">
+                      <p className="card-category">Reference Genome</p>
+                      {promiseInProgress === true ? (
+                        <LoadingIndicator />
+                      ) : (
+                        <CardTitle tag="p">{referenceSetName}</CardTitle>
+                      )}
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
           <Col lg="4" md="4" sm="4">
             <Card className="card-stats">
               <CardBody>
@@ -104,47 +158,32 @@ function VariantsSearch() {
               </CardBody>
             </Card>
           </Col>
-          <Col lg="4" md="4" sm="4">
-            <Card className="card-stats">
-              <CardBody>
-                <Row>
-                  <Col md="4" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-map-big text-primary" />
-                    </div>
-                  </Col>
-                  <Col md="8" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">ReferenceSet</p>
-                      {promiseInProgress === true ? (
-                        <LoadingIndicator />
-                      ) : (
-                        <CardTitle tag="p">{referenceSetName}</CardTitle>
-                      )}
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
         </Row>
         <Form inline onSubmit={formHandler} style={{ justifyContent: 'center' }}>
+          { options.length > 0
+            && (
+            <FormGroup>
+              <Label for="VariantSetIds">Variant Set</Label>
+              <MultiSelect // Width set in CSS
+                options={options}
+                value={selected}
+                onChange={setSelected}
+                labelledBy="Select"
+              />
+            </FormGroup>
+            )}
+          <FormGroup>
+            <Label for="referenceName">Chromosome</Label>
+            <Input required type="text" id="chromosome" />
+          </FormGroup>
           <FormGroup>
             <Label for="start">Start</Label>
             <Input required type="number" id="start" />
           </FormGroup>
-
           <FormGroup>
             <Label for="end">End</Label>
             <Input required type="number" id="end" />
           </FormGroup>
-
-          <FormGroup>
-            <Label for="referenceName">Reference Name</Label>
-            <Input required type="text" id="referenceName" />
-          </FormGroup>
-
           <Button>Search</Button>
         </Form>
 
