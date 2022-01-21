93b379be3e1c44646f0fbaa93c73a758e79cc516
Jimmy Li <lihaoyuan1996@gmail.com>
2022-01-21 04:58:50 -0800

    update reads table

---
+++ src/components/Tables/ReadsTable.js
@@ -0,0 +1,74 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { AgGridReact } from 'ag-grid-react';
+import { notify, NotificationAlert } from '../../utils/alert';
+import VariantsTableButton from './VariantsTableButton';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import '../../assets/css/VariantsSearch.css';
+
+function ReadsTable({ rowData, datasetId }) {
+  const notifyEl = useRef(null);
+
+  let gridOptions = {};
+
+  function getColumnDefs() {
+    const columnDefs = [
+      { headerName: 'Aligned Sequence', field: 'alignedSequence' },
+      { headerName: 'Fragment Name', field: 'fragmentName' },
+      { headerName: 'Number Reads', field: 'numberReads' },
+      { headerName: 'Fragment Length', field: 'fragmentLength' },
+      { headerName: 'Read Number', field: 'readNumber' },
+      { headerName: 'Improper Placement', field: 'improperPlacement' }
+    ];
+
+    return columnDefs;
+  }
+
+  gridOptions = {
+    defaultColDef: {
+      editable: false,
+      sortable: true,
+      resizable: true,
+      filter: true,
+      flex: 1,
+      minWidth: 150,
+      minHeight: 300,
+    },
+    rowSelection: 'single',
+    rowData: null,
+    rowGroupPanelShow: 'always',
+    pivotPanelShow: 'always',
+    enableRangeSelection: true,
+    paginationAutoPageSize: true,
+    pagination: true,
+    valueCache: true,
+    frameworkComponents: {
+      VariantsTableButton,
+    },
+  };
+
+  return (
+    <>
+      <NotificationAlert ref={notifyEl} />
+      <div className="ag-theme-alpine">
+        <AgGridReact
+          columnDefs={getColumnDefs()}
+          rowData={rowData}
+          gridOptions={gridOptions}
+          context={{ datasetId }}
+        />
+      </div>
+
+      <br />
+    </>
+  );
+}
+
+ReadsTable.propTypes = {
+  rowData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  datasetId: PropTypes.string.isRequired,
+};
+
+export default ReadsTable;
