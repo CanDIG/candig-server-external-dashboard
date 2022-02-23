43ee5f3c3a037c8dcbb307d9fc82486783b35a25
Jimmy Li <lihaoyuan1996@gmail.com>
2022-02-23 05:21:33 -0800

    fixed sidebar

--- src/components/Navbars/TopBar.js
+++ src/components/Navbars/TopBar.js
@@ -1,14 +1,13 @@
-/* eslint-disable */
 /*!
 
 =========================================================
-* Paper Dashboard React - v1.2.0
+* Paper Dashboard React - v1.3.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
 
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
 
 * Coded by Creative Tim
 
@@ -17,8 +16,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   Collapse,
   Navbar,
@@ -30,155 +29,127 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Container
-} from "reactstrap";
+  Container,
+} from 'reactstrap';
 
-import routes from "routes.js";
+import routes from '../../routes';
+import DatasetsDropdown from '../Dropdowns/DatasetsDropdown';
 
-import DatasetsDropdown from "../Dropdowns/DatasetsDropdown.js";
-
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      dropdownOpen: false,
-      color: "transparent"
-    };
-    this.toggle = this.toggle.bind(this);
-    this.dropdownToggle = this.dropdownToggle.bind(this);
-    this.sidebarToggle = React.createRef();
-  }
-  toggle() {
-    if (this.state.isOpen) {
-      this.setState({
-        color: "transparent",
-      });
+function Header(props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+  const [color, setColor] = React.useState('transparent');
+  const sidebarToggle = React.useRef();
+  const location = useLocation();
+  const destructedProps = props;
+  const toggle = () => {
+    if (isOpen) {
+      setColor('transparent');
     } else {
-      this.setState({
-        color: "dark",
-      });
+      setColor('dark');
     }
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
-  dropdownToggle(e) {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
-  getBrand() {
-    let brandName = "";
-    routes.map((prop, key) => {
+    setIsOpen(!isOpen);
+  };
+  const dropdownToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  const getBrand = () => {
+    let brandName = 'Default Brand';
+    routes.map((prop) => {
       if (window.location.href.indexOf(prop.layout + prop.path) !== -1) {
         brandName = prop.name;
       }
       return null;
     });
     return brandName;
-  }
-  openSidebar() {
-    document.documentElement.classList.toggle("nav-open");
-    this.sidebarToggle.current.classList.toggle("toggled");
-  }
+  };
+  const openSidebar = () => {
+    document.documentElement.classList.toggle('nav-open');
+    sidebarToggle.current.classList.toggle('toggled');
+  };
   // function that adds color dark/transparent to the navbar on resize (this is for the collapse)
-  updateColor() {
-    if (window.innerWidth < 993 && this.state.isOpen) {
-      this.setState({
-        color: "dark",
-      });
+  const updateColor = () => {
+    if (window.innerWidth < 993 && isOpen) {
+      setColor('dark');
     } else {
-      this.setState({
-        color: "transparent",
-      });
+      setColor('transparent');
     }
-  }
-  componentDidMount() {
-    window.addEventListener("resize", this.updateColor.bind(this));
-  }
-  componentDidUpdate(e) {
+  };
+  React.useEffect(() => {
+    window.addEventListener('resize', updateColor.bind(this));
+  });
+  React.useEffect(() => {
     if (
-      window.innerWidth < 993 &&
-      e.history.location.pathname !== e.location.pathname &&
-      document.documentElement.className.indexOf("nav-open") !== -1
+      window.innerWidth < 993
+      && document.documentElement.className.indexOf('nav-open') !== -1
     ) {
-      document.documentElement.classList.toggle("nav-open");
-      this.sidebarToggle.current.classList.toggle("toggled");
+      document.documentElement.classList.toggle('nav-open');
+      // sidebarToggle.current.classList.toggle('toggled');
     }
-  }
-  render() {
-    return (
-      // add or remove classes depending if we are on full-screen-maps page or not
-      <Navbar
-        color={
-          this.props.location.pathname.indexOf("full-screen-maps") !== -1
-            ? "dark"
-            : this.state.color
-        }
-        expand="lg"
-        className={
-          this.props.location.pathname.indexOf("full-screen-maps") !== -1
-            ? "navbar-absolute fixed-top"
-            : "navbar-absolute fixed-top " +
-              (this.state.color === "transparent" ? "navbar-transparent " : "")
-        }
-      >
-        <Container fluid>
-          <div className="navbar-wrapper">
-            <div className="navbar-toggle">
-              <button
-                type="button"
-                ref={this.sidebarToggle}
-                className="navbar-toggler"
-                onClick={() => this.openSidebar()}
-              >
-              
-              </button>
-            </div>
-            <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
+  }, [location]);
+  return (
+    // add or remove classes depending if we are on full-screen-maps page or not
+    <Navbar
+      color={
+        destructedProps.location.pathname.indexOf('full-screen-maps') !== -1
+          ? 'dark'
+          : color
+      }
+      expand="lg"
+      className={
+        destructedProps.location.pathname.indexOf('full-screen-maps') !== -1
+          ? 'navbar-absolute fixed-top'
+          : `navbar-absolute fixed-top ${
+            color === 'transparent' ? 'navbar-transparent ' : ''}`
+      }
+    >
+      <Container fluid>
+        <div className="navbar-wrapper">
+          <div className="navbar-toggle">
+            <button
+              type="button"
+              ref={sidebarToggle}
+              className="navbar-toggler"
+              onClick={() => openSidebar()}
+            >
+              <span className="navbar-toggler-bar bar1" />
+              <span className="navbar-toggler-bar bar2" />
+              <span className="navbar-toggler-bar bar3" />
+            </button>
           </div>
-          <NavbarToggler onClick={this.toggle}/>
-          <Collapse
-            isOpen={this.state.isOpen}
-            navbar
-            className="justify-content-end"
-          >
-            <Nav>
-              {this.props.datasetVisible === true ? 
-                (<DatasetsDropdown updateState={this.props.updateState} />) :
-                <></> }
-              {/* Leaving this commented out for now */}
-              <Dropdown
-                nav
-                isOpen={this.state.dropdownOpen}
-                toggle={(e) => this.dropdownToggle(e)}
-              >
-                <DropdownToggle caret nav>
-                  <i className="nc-icon nc-bell-55" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Account</span>
-                  </p>
-                </DropdownToggle>              
-                <DropdownMenu right>
-                  <DropdownItem href="/">Return to production dashboard</DropdownItem>
-                  <DropdownItem href="/auth/logout">Logout</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              {/* <NavItem>
-                <Link to="#pablo" className="nav-link btn-rotate">
-                  <i className="nc-icon nc-settings-gear-65" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Account</span>
-                  </p>
-                </Link>
-              </NavItem> */}
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+          <NavbarBrand href="/">{getBrand()}</NavbarBrand>
+        </div>
+        <NavbarToggler onClick={toggle}>
+          <span className="navbar-toggler-bar navbar-kebab" />
+          <span className="navbar-toggler-bar navbar-kebab" />
+          <span className="navbar-toggler-bar navbar-kebab" />
+        </NavbarToggler>
+        <Collapse isOpen={isOpen} navbar className="justify-content-end">
+          <Nav navbar>
+            {destructedProps.datasetVisible === true
+              ? (<NavItem><DatasetsDropdown updateState={destructedProps.updateState} /></NavItem>)
+              : <></> }
+            <Dropdown
+              nav
+              isOpen={dropdownOpen}
+              toggle={(e) => dropdownToggle(e)}
+            >
+              <DropdownToggle caret nav>
+                <i className="nc-icon nc-bell-55" />
+                <p>
+                  <span className="d-lg-none d-md-block">Account</span>
+                </p>
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href="/">Return to production dashboard</DropdownItem>
+                <DropdownItem href="/auth/logout">Logout</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
